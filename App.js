8969537a5f78bd83c6148ffd8d1ded630f62830d
Idit Aditya <iditlp3i@gmail.com>
2021-01-18 16:23:33 +0700

    add splash

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack' ;
 import {NavigationContainer} from '@react-navigation/native' ;
+import {createDrawerNavigator} from '@react-navigation/drawer' ;
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs' ;
 
-
-import { signIn, createAccount} from './screens/homeScreens' ;
+import {signIn, 
+        createAccount,
+        Home,
+        Profile,
+        Splash,
+        Search,
+        Search2,
+        Details
+    } from './screens/homeScreens' ;
 
 const AuthStack = createStackNavigator();
 
+
+
+
 export default() => {
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(()=>{
+    setTimeout(()=>{
+      setIsLoading();
+    },1000);
+  }, []);
+
+  if (isLoading){
+      return <Splash />
+  }
+
   return (
     <NavigationContainer>
       <AuthStack.Navigator>
--- screens/homeScreens.js
+++ screens/homeScreens.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
 
 export const signIn = ({navigation}) => {
 return(
@@ -9,24 +9,92 @@ return(
         <TouchableOpacity onPress={() => Navigation.push()}>
             <Text style={styles.textColor}>Sign in</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.push("createAccount")}>
+        <TouchableOpacity onPress={() => navigation.navigate("createAccount")}>
             <Text style={styles.textColor}>create Account</Text>
         </TouchableOpacity>
     </View>
 );
 }
 
-export const createAccount = () => {
+export const createAccount = ({navigation}) => {
     return(
         <View style={styles.container}>
             <Text>CreateAccount Screens</Text>
-            <TouchableOpacity onPress={() => navigation.push()}>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
                 <Text style={styles.textColor}>Sign Out</Text>
             </TouchableOpacity>
         </View>
     );
 }
 
+export const Profile = () => {
+    return(
+        <View style={styles.container}>
+            <Text>Profile Screens</Text>
+            <TouchableOpacity>
+                <Text>Drawer</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text>Sign Out</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+export const Splash = () => {
+    return(
+        <View style={styles.container}>
+            <ActivityIndicator size="large" color="#00ff00" />
+        </View>
+    );
+}
+
+export const Home = () => {
+    return(
+        <View style={styles.container}>
+            <Text>Master list Screens</Text>
+            <TouchableOpacity
+            ><Text style={styles.textColor}>React native by me</Text>   
+            </TouchableOpacity>
+            <TouchableOpacity
+            ><Text style={styles.textColor}>React Native example</Text>   
+            </TouchableOpacity>
+            <TouchableOpacity
+            ><Text style={styles.textColor}>Detail</Text>   
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+export const Details = () => {
+    return(
+        <View style={styles.container}>
+            <Text>Details Screens</Text>
+        </View>
+    );
+}
+
+export const Search = () => {
+    return(
+        <View style={styles.container}>
+            <Text>Search Screens</Text>
+            <TouchableOpacity>
+                <Text>Search</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text>React Native Example</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+export const Search2 = () => {
+    return(
+        <View style={styles.container}>
+            <Text>Search Screens</Text>
+        </View>
+    );
+}
 
 
 const styles = StyleSheet.create({
